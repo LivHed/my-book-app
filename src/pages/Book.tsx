45ff40279920a32ba54cc48faf8066347a335c6f
LivHed <livia.hedin@gmail.com>
2023-06-20 15:47:42 +0200

    Handle to get one book with fetch and display the id on the page

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+
+interface OneBook {
+    id: string,
+    name: string,
+    genre: string,
+    coverUrl: string,
+    description: string,
+    averageRating: number,
+    haveRead: number,
+    currentlyReading: number,
+    wantToRead: number,
+    userRating: number
+}
 
 const Book = () => {
+    const [oneBook, setOneBook] = useState<OneBook[]>([])
+    const [error, setError] = useState({})
+    // Get the userId param from the URL
+    let { id } = useParams();
+
+    useEffect(() => {
+        fetch('https://devies-reads-be.onrender.com/books/' + id)
+            .then(response => response.json())
+            .then(res => setOneBook(res))
+            .catch(err => setError(err))
+    }, [])
+
     return (<>
-        <h1>More info about this book</h1>
+        <h1>More info about this book: </h1>
+        <div>{id}</div>
     </>)
 }
 
